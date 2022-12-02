72639da8d548e7fee1e09ec7231925152329fce5
EduardUrakov <eduard.urakov@mail.ru>
2022-12-02 10:05:25 +0500

    take names

---
+++ Exemples5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("You name please ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ed")
+{
+    Console.WriteLine("You are wellcome!!!");
+}
+else
+{
+    Console.Write("Hi!!!");
+    Console.WriteLine(username);
+}
